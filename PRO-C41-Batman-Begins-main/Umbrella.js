f8944e065297c2db25ffb7a6e377b0dc772b1f83
RitikaPunjabi <69786032+RitikaPunjabi@users.noreply.github.com>
2021-09-12 12:54:06 +0530

    Add files via upload

---
+++ PRO-C41-Batman-Begins-main/Umbrella.js
@@ -0,0 +1,23 @@
+class Umbrella{
+    constructor(x,y){
+
+        var options ={
+            isStatic: true
+        }
+        this. r = 50;
+        
+        this.body = Bodies.circle(x,y,50,options);
+        this.image = loadImage("images/Walking Frame/walking_1.png");
+        World.add(world,this.body);
+    }
+
+    
+
+    display(){
+        var pos = this.body.position;
+       
+      
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
+}
